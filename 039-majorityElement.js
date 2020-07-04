d91d42656a68df34d6e99c43b6c0b0b220054314
bblon <jq93006217@163.com>
2020-07-04 19:28:56 +0800

    add: majorityElement

---
+++ 039-majorityElement.js
@@ -0,0 +1,25 @@
+/**
+ * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * 
+ * 示例 1:
+ * 输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+ * 输出: 2
+ * 
+ * 限制：
+ * 1 <= 数组长度 <= 50000
+ */
+
+
+ /**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  let votes = 0, x = null;
+  for(let i = 0; i < nums.length; i++) {
+    if(votes === 0) x = nums[i];
+    votes += x === nums[i] ? 1 : -1;
+  }
+  return x;
+};
