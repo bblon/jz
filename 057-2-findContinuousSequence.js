60f7486aa26a9cf939af8f0a3077a394e1dcae23
bblon <jq93006217@163.com>
2020-07-08 20:02:08 +0800

    add: findContinuousSequence

---
+++ 057-2-findContinuousSequence.js
@@ -0,0 +1,43 @@
+/**
+ * 输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+ * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+ * 
+ * 示例 1：
+ * 输入：target = 9
+ * 输出：[[2,3,4],[4,5]]
+ * 
+ * 示例 2：
+ * 输入：target = 15
+ * 输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+ * 
+ * 限制：
+ * 1 <= target <= 10^5
+ */
+
+ /**
+ * @param {number} target
+ * @return {number[][]}
+ */
+var findContinuousSequence = function(target) {
+  let [i, j] = [1, 1];
+  let sum = 0;
+  let res = [];
+  while(j <= Math.ceil(target / 2)) {
+    if(sum < target) {
+      sum += j;
+      j++;
+    } else if(sum > target) {
+      sum -= i;
+      i++;
+    } else {
+      let t = [];
+      for(let k = i; k <= j; k++) {
+        t.push(k);
+      }
+      res.push(t);
+      sum -= i;
+      i++;
+    }
+  }
+  return res;
+};
